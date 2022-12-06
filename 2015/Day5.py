1f62f0daa546dd4b89c820170ffb5d9b45001752
Carl Rezelman <95653864+CRezelman@users.noreply.github.com>
2022-12-06 21:43:22 +0200

    hacky method for day 5 part2; this problem is probably better solved using a grep command in linux...

--- 2015/Day5.py
+++ 2015/Day5.py
@@ -4,10 +4,14 @@ def day5():
     notAllowed = ['ab', 'cd', 'pq', 'xy']
     vowels = ['a', 'e', 'i', 'o', 'u']
     with open('2015/day5.txt') as f:
-        for line in f:
+        for ind, line in enumerate(f):
+            line = line.strip('\n')
             vowelsFound = 0
             doubleFound = 0
             badFound = 0
+            pairsFound = 0
+            repeatsSplitFound = 0
+            pairSet = set()
             prevChar = ''
             for bad in notAllowed:
                 if bad in line:
@@ -22,15 +26,29 @@ def day5():
         
             if badFound == 0 and vowelsFound >= 3 and doubleFound:
                 nice +=1
+            triples = 0
+            for i, char in enumerate(line):
+                if i >= 1:
+                    substring = line[i-1:i+1]
+                    pairSet.add(substring)
+                if i >= 2:
+                    sub = line[i-2:i+1]
+                    if sub[0] == sub[1] and sub[1] == sub[2]:
+                        triples +=1
 
-            # for i, char in enumerate(line):
-            #     if(i <= 13):
-            #         if line[i] == line[i+1]:
-            #             substr = line[i] + line[i+1]
-            #             if substr in line[2:len(line)]:
-            #                 nice2 += 1
-            #         elif line[i] == line[i+2]:
-            #             nice2 +=1
+            if len(pairSet) < ((len(line)-1) - triples):
+                pairsFound += 1
+
+
+            for i, char in enumerate(line):
+                if i >= 2:
+                    substring = line[i-2:i+1]
+                    if substring[0] == substring[2]:
+                        repeatsSplitFound = 1
+            
+            if pairsFound and repeatsSplitFound:
+                nice2 += 1
+                
 
     return nice, nice2
 
