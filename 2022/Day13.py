251290aea689bb4cedc364fdedb65b54cd835cd0
Carl Rezelman <95653864+CRezelman@users.noreply.github.com>
2022-12-13 15:15:38 +0200

    day13 part2 solve

--- 2022/Day13.py
+++ 2022/Day13.py
@@ -1,21 +1,15 @@
 import ast
+from functools import cmp_to_key
 
-class Pair:
-    def __init__(self, first, second):
-        self.first = first
-        self.second = second
-
-
-def compare(first, second, index):
+def compare(first, second):
 
     if isinstance(first, int) and isinstance(second, int):
         if (first < second):
-            print(index)
-            return index
+            return 1
         elif (first > second):
-            return False
-        elif (first == second):
             return -1
+        elif (first == second):
+            return 0
 
     if isinstance(first, list) and isinstance(second, int):
         second = [second]
@@ -26,39 +20,55 @@ def compare(first, second, index):
     if isinstance(first, list) and isinstance(second, list):
         minLength = min(len(first), len(second))
 
-        comp = -1
+        comp = 0
         for i in range(minLength):
-            comp = compare(first[i], second[i], index)
-            if comp != -1:
+            comp = compare(first[i], second[i])
+            if comp != 0:
                 return comp
 
-        if (comp == -1):
+        if (comp == 0):
             if len(first) > len(second):
-                return False
-            elif len(first) < len(second):
-                print(index)
-                return index
-            elif len(first) == len(second):
                 return -1
+            elif len(first) < len(second):
+                return 1
+            elif len(first) == len(second):
+                return 0
 
 
 def day13():
     part1 = 0
-    pairs = []
+    part2 = 0
+    messages1 = []
+    messages2 = [[[2]], [[6]]]
+
     with open('2022/inputs/day13.txt') as f:
         for i, line in enumerate(f):
             line = line.strip('\n')
             if i % 3 == 0:
                 first = ast.literal_eval(line)
+                messages2.append(ast.literal_eval(line))
             elif i % 3 == 1:
                 second = ast.literal_eval(line)
+                messages2.append(ast.literal_eval(line))
             elif i % 3 == 2:
-                pairs.append(Pair(first, second))
+                messages1.append([first, second])
 
 
-    for j, pair in enumerate(pairs):
-        part1 += compare(pair.first, pair.second, j+1)
+    for j, msg in enumerate(messages1):
+        comp = compare(msg[0], msg[1])
+        if comp > -1:
+            part1 += (j+1)
+    
+    messages2 = sorted(messages2, key=cmp_to_key(compare), reverse=1)  
 
-    return part1
+    part2 = 1
+    for n, msg in enumerate(messages2):
+        print(msg)
+        if msg == [[2]]:
+            part2 *= (n+1)
+        if msg == [[6]]:
+            part2 *= (n+1)
+
+    return part1, part2
 
 print(day13())
