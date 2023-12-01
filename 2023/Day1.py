ed4136f705a4eec16f45f2cf573b177a9a7b1dc9
Carl Rezelman <carlrez26@gmail.com>
2023-12-01 09:00:39 +0200

    improve day 1 solve

--- 2023/Day1.py
+++ 2023/Day1.py
@@ -1,45 +1,31 @@
-
-def day1():
-    result = 0
-    num1 = 0
-    num2 = 0
+def processLine(line: str, part: int, reverse = False) -> str:
     numChars = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    num = 0
+    substr = ''
+    line = line[::-1] if reverse else line
+    
+    for char in line:
+        if num and part == 2: break
+        substr += char
+        for i, numChar in enumerate(numChars):
+            if numChar in (substr[::-1] if reverse else substr):
+                num = str(i + 1)
+                break
+
+        if char.isdigit():
+            num = char
+            break
+    return num
+
+def day1(part: int) -> int:
+    result = 0
     with open('2023/inputs/day1.txt') as f:
         for line in f:
             line = line.strip('\n')
-            left = ''
-            for char in line:
-                if num1: break
-                left += char
-                for i, numChar in enumerate(numChars):
-                    if numChar in left:
-                        num1 = str(i+1)
-                        break
-                if char.isdigit():
-                    num1 = char
-                    break
- 
-            line = line[::-1]
-            right = ''
-            for char in line:
-                if num2: break
-                right += char
-                for i, numChar in enumerate(numChars):
-                    revChar = right[::-1]
-                    if numChar in revChar:
-                        num2 = str(i+1)
-                        break
-                if char.isdigit():
-                    num2 = char
-                    break
-    
-                    
-                
-            num = int(str(num1) + str(num2))
-            num1 = 0 
-            num2 = 0 
-            result += num
+            num1 = processLine(line, part, False)
+            num2 = processLine(line, part, True)
+            result += int(str(num1) + str(num2))
 
     return result
 
-print(day1())
+print(day1(1), day1(2))
