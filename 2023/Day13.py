c2abebd81d3815e0e07b8eb9b7fa7be1d76d2bae
Carl Rezelman <carlrez26@gmail.com>
2023-12-14 00:06:39 +0200

    2023 day 13 full optimised solve

--- 2023/Day13.py
+++ 2023/Day13.py
@@ -1,34 +1,27 @@
 
-def checkSymmetry(mirror):
+def checkSymmetry(mirror: list[str], allowedDiff: int):
     for y in range(len(mirror) - 1):
         dy = min(y, len(mirror) - y - 2)
-        for j in range(dy + 1):
-            if mirror[y - j] != mirror[y + j + 1]:
-                break
-        else:
-            return y+1
-    return False
+        counts = [countDiff(mirror[y - j], mirror[y + j + 1]) for j in range(dy + 1)]
+
+        if sum(counts) == allowedDiff:
+            return y + 1
+    return 0
+
+
+def countDiff(mirror1: str, mirror2: str):
+    if mirror1 == mirror2: return 0
+    return sum(char1 != char2 for char1, char2 in zip(mirror1, mirror2))
 
 
 def day13(): 
     part1 = 0
     part2 = 0
-    mirrors = []
-    with open('2023/inputs/day13.txt', 'r') as f:
-        currentMirror = []
-        for line in f:
-                if line == '\n':
-                    mirrors.append(currentMirror)
-                    currentMirror = []
-                else:
-                    currentMirror.append(line.strip())
-        mirrors.append(currentMirror)
 
-
-    for mirror in mirrors:
-        part1 += 100*(int(checkSymmetry(mirror)))
-        transpose = list(zip(*mirror))
-        part1 += int(checkSymmetry(transpose))
+    for block in open('2023/inputs/day13.txt').read().split('\n\n'):
+        mirror = block.splitlines()
+        part1 += 100*(checkSymmetry(mirror, 0)) + checkSymmetry(list(zip(*mirror)), 0)
+        part2 += 100*(checkSymmetry(mirror, 1)) + checkSymmetry(list(zip(*mirror)), 1)
 
     return part1, part2
 
