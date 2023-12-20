d1f193953843cb8bd49903184941864a21d27885
Carl Rezelman <carlrez26@gmail.com>
2023-12-20 20:47:10 +0200

    optimise solution

--- 2023/Day20.py
+++ 2023/Day20.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractclassmethod
-from copy import deepcopy
+from collections import deque
 import math
 
 class Module(ABC):
@@ -41,72 +41,69 @@ class Conjunction(Module):
         return not all(value == True for key, value in self.inputs.items())
 
         
-def solve(iterations: int, modules: dict[str, Module]):
-    high = 0
-    low = 0
+def solve(modules: dict[str, Module]):
+    high = low = 0
     moduleRx = {}
-    for i in range(iterations):
+
+    for i in range(5000):
+        if i == 1000:
+            part1 = high*low
         low += 1
         currentModule = modules['broadcaster']
         signal = currentModule.updateState(0, None)
-        q: list[tuple[Module, bool]] = [(currentModule, signal)]
-        
-        while len(q):
-            currentModule, signal = q.pop(0)
+        q = deque([(currentModule, signal)])
+
+        while q:
+            currentModule, signal = q.popleft()
             for con in currentModule.connections:
                 if signal:
                     high += 1
                 else:
                     low += 1
+
                 if modules.get(con):
-                    if signal == True and modules[con].id == 'zp':
+                    if signal and modules[con].id == 'zp':
                         if not moduleRx.get(currentModule.id):
                             moduleRx[currentModule.id] = i + 1
-                        if len(moduleRx) == len(modules[con].inputs):
-                            return math.lcm(*moduleRx.values())
+                        if set(moduleRx) == set(modules[con].inputs):
+                            return part1, math.lcm(*moduleRx.values())
+
                     newSignal = modules[con].updateState(signal, currentModule.id)
+
                     if newSignal is not None:
                         q.append((modules[con], newSignal))
-    
-    return high*low
+
+    return high * low
+
+def parseInput(inputFile, modules, callback):
+    for line in open(inputFile, 'r').read().splitlines():
+        module, connections = line.strip().split(' -> ')
+        connections = connections.split(', ')
+        id = module.strip('&%')
+        callback(modules, module, id, connections)
+
+def initModules(modules, module, id, connections):
+    if '%' in module:
+        current = FlipFlop(id, connections)
+    elif '&' in module:
+        current = Conjunction(id, connections)
+    else:
+        current = BroadCaster(connections)
+
+    modules[id] = current
+
+def initInputs(modules: dict[str, Module], module: Module, id, connections):
+    for con in connections:
+        if modules.get(con):
+            modules[con].inputs[id] = False
         
 
 def day20():
     inputFile = '2023/inputs/day20.txt'
+    modules: dict[str, Module] = {}
+    parseInput(inputFile, modules, initModules)
+    parseInput(inputFile, modules, initInputs)
 
-    modules: dict[str, BroadCaster | FlipFlop | Conjunction] = {}
-    modulesCopy: dict[str, BroadCaster | FlipFlop | Conjunction] = {}
-
-    with open(inputFile, 'r') as f:
-        for line in f:
-            module, connections = line.strip().split(' -> ')
-            connections = connections.split(', ')
-            id = module.strip('&%')
-
-            if '%' in module:
-                current = FlipFlop(id, connections)
-            elif '&' in module:
-                current = Conjunction(id, connections)
-            else:
-                id = 'broadcaster'
-                current = BroadCaster(connections)
-            
-            modules[id] = current
-
-        with open(inputFile, 'r') as f:
-            for line in f:
-                module, connections = line.strip().split(' -> ')
-                id = module.strip('%&')
-
-                for con in connections.split(', '):
-                    if modules.get(con):
-                        modules[con].inputs[id] = False
-
-        modulesCopy = deepcopy(modules)
-
-    part1 = solve(1000, modules)
-    part2 = solve(5000, modulesCopy)
-
-    return part1, part2
+    return solve(modules)
 
 print(day20())
