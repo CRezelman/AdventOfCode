8b0efccbfa6bd2d9a84b2e8c543ffc9dc7c27176
Carl Rezelman <carl.rezelman@vastech.co.za>
2025-12-02 12:56:15 +0200

    Framework improvements

--- 2025/Day2.py
+++ 2025/Day2.py
@@ -2,20 +2,20 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utilities.input import read_lines
+from utilities.input import read_line
 
 def day2():
     """Day 2"""
     part1 = 0
     part2 = 0
-    lines = read_lines(2025, 2)
+    line = read_line(2025, 2)
 
-    values = [x for x in lines[0].split(',')]
+    values = [x for x in line.split(',')]
 
     for v in values:
-        min, max = v.split('-')
+        low, high = map(int, v.split('-'))
 
-        for id in range(int(min), int(max)+1):
+        for id in range(low, high + 1):
             id_str = str(id)
             length = len(id_str)
             found_ids = set()
@@ -27,12 +27,11 @@ def day2():
 
                 substrings = [id_str[i*size:(i+1)*size] for i in range(div)]
 
-                if div == 2:
-                    if substrings[0] == substrings[1]:
-                        part1 += id
-
                 if all(sub == substrings[0] for sub in substrings):
                     found_ids.add(id)
+                    if div == 2:
+                        part1 += id
+
             part2 += sum(found_ids)
 
     return part1, part2
--- utilities/input.py
+++ utilities/input.py
@@ -1,9 +1,13 @@
 """Utility File for reading Input Data"""
 
+def read_line(year: int, day: int, demo: bool = False) -> str:
+    """Reads a file and returns a single line"""
+    return open(f'{year}/inputs/day{day}{"demo" if demo else ""}.txt', 'r', encoding='utf-8').read().strip()
+
 def read_lines(year: int, day: int, demo: bool = False) -> list[str]:
     """Reads a file and returns a list of lines"""
-    return open(f'{year}/inputs/day{day}{"demo" if demo else ""}.txt', 'r', encoding='utf-8').read().strip().split('\n')
+    return open(f'{year}/inputs/day{day}{"demo" if demo else ""}.txt', 'r', encoding='utf-8').read().strip().splitlines()
 
 def read_grid(year: int, day: int, demo: bool = False) -> list[list[str]]:
     """Reads a file and returns a list of lists"""
-    return [list(line) for line in open(f'{year}/inputs/day{day}{"demo" if demo else ""}.txt', 'r', encoding='utf-8').read().strip().split('\n')]
+    return [list(line) for line in open(f'{year}/inputs/day{day}{"demo" if demo else ""}.txt', 'r', encoding='utf-8').read().strip().splitlines()]
