94fc1d0e3b22e00f13c02f128f119cf7882dc786
Carl Rezelman <carl.rezelman@vastech.co.za>
2025-12-05 09:53:53 +0200

    2025 Day 5 Improvements

--- 2025/Day5.py
+++ 2025/Day5.py
@@ -2,43 +2,27 @@
 from utilities.solver import InputType, Solver
 
 class Day5(Solver):
-
-    def parse_input(self) -> None:
-        self.fresh_range = []
-        self.available = []
-
-        found_new_line = False
-        for line in self.lines:
-            if line == "":
-                found_new_line = True
-                continue
-            if found_new_line:
-                self.available.append(int(line.strip()))
-            else:
-                min, max = line.strip().split('-')
-                self.fresh_range.append((int(min), int(max)))
-
     def solve(self) -> None:
-        self.parse_input()
+        range_lines, value_lines = "\n".join(self.lines).split("\n\n")
+        self.fresh_ranges: list[tuple[int, int]] = [
+            tuple(map(int, line.split('-'))) 
+            for line in range_lines.splitlines()
+        ]
+        self.available: list[int] = [
+            int(line) for line in value_lines.splitlines()
+        ]
 
-        for available_id in self.available:
-            for min_id, max_id in self.fresh_range:
-                if min_id <= available_id <= max_id:
-                    self.part1 += 1
-                    break
+        self.fresh_ranges = sorted(self.fresh_ranges, key=lambda x: x[0])
+        self.merged_fresh_ranges = [self.fresh_ranges[0]]
 
-        sorted_ranges = sorted(self.fresh_range, key=lambda x: x[0])
-        merged_ranges = [sorted_ranges[0]]
-
-        for current in sorted_ranges:
-            previous = merged_ranges[-1]
+        for current in self.fresh_ranges:
+            previous = self.merged_fresh_ranges[-1]
             if current[0] <= previous[1]:
-                merged_ranges[-1] = (previous[0], max(previous[1], current[1]))
+                self.merged_fresh_ranges[-1] = (previous[0], max(previous[1], current[1]))
             else:
-                merged_ranges.append(current)
-            
-        for min_id, max_id in merged_ranges:
-            self.part2 += (max_id - min_id + 1)
+                self.merged_fresh_ranges.append(current)
 
+        self.part1 = sum(any(min_id <= available_id <= max_id for min_id, max_id in self.merged_fresh_ranges) for available_id in self.available)
+        self.part2 = sum(max_id - min_id + 1 for min_id, max_id in self.merged_fresh_ranges)
     
 Day5(2025, 5, InputType.LINES).run()
