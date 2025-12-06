6a4483da9fcca1c89a0e0f0e1186bd25498a0c24
Carl Rezelman <carl.rezelman@vastech.co.za>
2025-12-07 00:32:57 +0200

    2025 Day 6 improvements

--- 2025/Day6.py
+++ 2025/Day6.py
@@ -5,33 +5,24 @@ import math
 class Day6(Solver):
 
     def solve(self) -> None:
-        ops = {
-            "*": lambda xs: math.prod(map(int, xs)),
-            "+": lambda xs: sum(map(int, xs)),
-        }
-
         self.problems = [line.split() for line in self.lines]
-        
-        for column_items in zip(*self.problems):
-            operation = column_items[-1]
-            self.part1 += ops[operation](column_items[:-1])
 
+        for *nums, op in zip(*self.problems):
+            self.part1 += eval(op.join(nums))
 
-        current_numbers = []
-        operation = None
+        group = []
+        op = None
         for column in reversed(list(zip(*self.grid.grid))):
             if all(c == " " for c in column):
                 continue
 
-            if column[-1] != " ":
-                operation = column[-1]
+            op = column[-1]
+            group.append("".join(column[:-1]).strip())
 
-            current_numbers.append("".join(column[:-1]).strip())
-
-            if operation:
-                self.part2 += ops[operation](current_numbers)
-                operation = None
-                current_numbers = []
+            if op != " " and op is not None:
+                self.part2 += eval(op.join(group))
+                op = None
+                group = []
 
     
 Day6(2025, 6, strip=False).run()
