21e9f10400657705a46bd886aef62e81f3dd79b5
Carl Rezelman <carl.rezelman@vastech.co.za>
2025-12-06 22:56:02 +0200

    2025 Day 6 improvement

--- 2025/Day6.py
+++ 2025/Day6.py
@@ -5,41 +5,32 @@ import math
 class Day6(Solver):
 
     def solve(self) -> None:
-        self.problems = []
+        ops = {
+            "*": lambda xs: math.prod(map(int, xs)),
+            "+": lambda xs: sum(map(int, xs)),
+        }
 
-        for line in self.lines:
-            items = line.split()
-            self.problems.append(items)
-
-        cols = len(self.problems[0]) 
+        self.problems = [line.split() for line in self.lines]
         
-        for col in range(cols):
-            column_items = [self.problems[row][col] for row in range(len(self.problems)) if col < len(self.problems[row])]
-            operation = column_items.pop(-1)
-            if operation == "*":
-                self.part1 += math.prod(map(int, column_items))
-            elif operation == "+":
-                self.part1 += sum(map(int, column_items))
+        for column_items in zip(*self.problems):
+            operation = column_items[-1]
+            self.part1 += ops[operation](column_items[:-1])
 
 
         current_numbers = []
-        current_operation = None
+        operation = None
         for column in reversed(list(zip(*self.grid.grid))):
             if all(c == " " for c in column):
                 continue
 
             if column[-1] != " ":
-                current_operation = column[-1]
+                operation = column[-1]
 
             current_numbers.append("".join(column[:-1]).strip())
 
-            if current_operation == "*":
-                self.part2 += math.prod(map(int, current_numbers))
-                current_operation = None
-                current_numbers = []
-            elif current_operation == "+":
-                self.part2 += sum(map(int, current_numbers))
-                current_operation = None
+            if operation:
+                self.part2 += ops[operation](current_numbers)
+                operation = None
                 current_numbers = []
 
     
