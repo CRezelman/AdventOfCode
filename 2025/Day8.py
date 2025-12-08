c1e3f62efa39fc1f2792d62f219a7bb22c038b3b
Carl Rezelman <carl.rezelman@vastech.co.za>
2025-12-08 17:02:18 +0200

    2025 Day 8 Full Solve

--- 2025/Day8.py
+++ 2025/Day8.py
@@ -4,43 +4,27 @@ import math
 import itertools
 
 class Day8(Solver):
-
-    def calculate_distance(self, j1, j2):
-        return math.sqrt(math.pow(abs(j1[0] - j2[0]), 2) + math.pow(abs(j1[1] - j2[1]), 2) + math.pow(abs(j1[2] - j2[2]), 2))
-
     def solve(self) -> None:
-        self.junctions = set()
-        for line in self.lines:
-            self.junctions.add(tuple(int(x) for x in line.split(',')))
-
-        distance_map = {}
-
-        for j1, j2 in itertools.combinations(self.junctions, 2):
-            if j1 == j2:
-                continue
-            distance = self.calculate_distance(j1, j2)
-            distance_map[(j1, j2)] = distance
-
-        distance_items = sorted(distance_map.items(), key=lambda x: x[1])
-
+        self.junctions = {tuple(int(x) for x in line.split(',')) for line in self.lines}
         self.circuits = [set([j]) for j in self.junctions]
-        connections = 0
-        MAX_CONNECTIONS = 1000
-        for (j1, j2), distance in distance_items:
-            if connections >= MAX_CONNECTIONS:
-                break
+
+        self.distance_map = {(j1, j2): math.dist(j1, j2) for j1, j2 in itertools.combinations(self.junctions, 2)}
+        self.distance_map = sorted(self.distance_map.items(), key=lambda x: x[1])
+
+        for i, ((j1, j2), _) in enumerate(self.distance_map):
+            if i == 1000:
+                self.circuits.sort(key=lambda x: len(x), reverse=True)
+                self.part1 = math.prod(len(circuit) for circuit in self.circuits[:3])
 
             j1_circuit = next(circuit for circuit in self.circuits if j1 in circuit)
             j2_circuit = next(circuit for circuit in self.circuits if j2 in circuit)
 
-            connections += 1
             if j1_circuit != j2_circuit:
                 j1_circuit.update(j2_circuit)
                 self.circuits.remove(j2_circuit)
+                if len(self.circuits) == 1:
+                    self.part2 = j1[0] *j2[0]
+                    break
                 continue
-
-        self.circuits.sort(key=lambda x: len(x), reverse=True)
-        self.part1 = math.prod(len(circuit) for circuit in self.circuits[:3])
-
     
 Day8(2025, 8).run()
